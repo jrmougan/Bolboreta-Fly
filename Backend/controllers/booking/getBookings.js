2b40084f8d7d2383f86ad6f03543e326551a7e5e
eduardo <eduardsanchoforeman@hotmail.com>
2021-12-15 14:02:36 +0100

    Pendiente de modificaciones

--- Backend/controllers/booking/getBookings.js
+++ Backend/controllers/booking/getBookings.js
@@ -1,16 +1,27 @@
 const getDB = require('../../database/getDB');
-const { AMADEUS_ID, AMADEUS_SECRET } = process.env;
-const Amadeus = require('amadeus');
-const amadeus = new Amadeus({
-    clientId: AMADEUS_ID,
-    clientSecret: AMADEUS_SECRET,
-});
+
 const getBookings = async (req, res, next) => {
     let connection;
     try {
+        connection = await getDB();
+
+        // Obtenemos el usuario objeto de consulta para conseguir sus reservas
+        const { userId } = req.params;
+
+        const [bookings] = await connection.query(
+            `
+            SELECT u.id AS ID_USUARIO, b.id AS ID_RESERVA,departure_terminal AS 'Desde',arrival_terminal AS 'A', booking_code AS 'Código de reserva', creation_date AS 'Fecha de creación', final_price AS 'Precio Final', departure_time AS 'Fecha de salida', arrival_time AS 'Fecha de llegada' 
+            FROM user AS u INNER JOIN booking AS b 
+            ON u.id= b.id_user
+            INNER JOIN passenger_rel_flight_rel_booking AS ternario ON ternario.booking_id=b.id 
+            WHERE u.id=2
+            ;`,
+            [userId]
+        );
+
         res.send({
             status: 'ok',
-            data: 'result',
+            data: bookings,
         });
     } catch (error) {
         next(error);
