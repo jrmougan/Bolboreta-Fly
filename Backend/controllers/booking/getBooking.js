a86f8c5a2eab900ca95c88c688429429fe43910e
eduardo <eduardsanchoforeman@hotmail.com>
2021-12-20 10:54:48 +0100

    Mejoras getBooking

--- Backend/controllers/booking/getBooking.js
+++ Backend/controllers/booking/getBooking.js
@@ -9,11 +9,20 @@ const getBookings = async (req, res, next) => {
 
         const [booking] = await connection.query(
             `
-        SELECT DISTINCT b.booking_code AS 'Code', b.final_price, b.currency, ternario.flight_id, f.departure_code, f.arrival_code,f.duration, f.flight_num, b.creation_date
+                    SELECT DISTINCT b.booking_code AS 'Localizador',
+                    COUNT(ternario.passenger_id) AS 'Pasajeros',
+                    CONCAT(b.final_price,' ', + b.currency) AS 'Precio Final',
+                    f.departure_code AS 'Salida',
+                    f.arrival_code AS 'Llegada',
+                    f.duration,
+                    f.flight_num,
+                    b.creation_date,
+                    COUNT(ternario.passenger_id) AS 'Pasajeros'
         FROM booking b
         LEFT JOIN passenger_rel_flight_rel_booking AS ternario ON b.id=ternario.booking_id
         LEFT JOIN flight as f ON ternario.flight_id=f.id
         WHERE booking_id=?
+        GROUP BY f.id;
         `,
             [bookingId]
         );
