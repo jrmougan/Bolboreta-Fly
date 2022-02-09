20d1f36e23a5d54aa097e2785d4c3e0dd9a68902
MartaPadin <martapadingomez@gmail.com>
2022-02-09 12:07:40 +0100

    arreglo de stilos

--- Frontend/src/page/LoginScreen.js
+++ Frontend/src/page/LoginScreen.js
@@ -3,7 +3,9 @@ import LoginUser from "../components/LoginUser/LoginUser";
 const LoginScreen = () => {
   return (
     <div>
-      Login
+      <h3> Si eres usuario registrado de Bolboreta Flight.
+      </h3>
+      <h1> Inicia Sesión</h1>
       <LoginUser />
     </div>);
 
