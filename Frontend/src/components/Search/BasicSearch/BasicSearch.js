5a7fb3696ada9e9ff6637221e5725c53a8015dcf
eduardo <eduardsanchoforeman@hotmail.com>
2022-02-17 13:32:39 +0100

    Mejoras en el index e iconos añadidos

--- Frontend/src/components/Search/BasicSearch/BasicSearch.js
+++ Frontend/src/components/Search/BasicSearch/BasicSearch.js
@@ -4,8 +4,11 @@ import {
   FaPlaneDeparture,
   FaPlaneArrival,
   AiOutlineUser,
+  FaWpforms,
+  FaUserFriends,
 } from 'react-icons/fa';
 import NavLinks from '../../NavLinks/NavLinks';
+import { ADULTS } from '../../StepperForm/InfoFlights/constantInfo';
 
 export const BasicSearch = ({
   search,
@@ -25,45 +28,54 @@ export const BasicSearch = ({
       <NavLinks />
       <form onSubmit={search}>
         <div className='inputsFlight'>
-          <input
-            type='text'
-            className='searchInput searchInput-1'
-            id='origin'
-            value={origin}
-            onChange={handleSubmit(setOrigin)}
-            placeholder='Elija una ciudad'
-          ></input>
+          <div className='inputDiv'>
+            <FaPlaneDeparture className='faplane_icon' />
+            <input
+              type='text'
+              className='searchInput searchInput-1'
+              id='origin'
+              value={origin}
+              onChange={handleSubmit(setOrigin)}
+            ></input>
+          </div>
+          <div className='inputDiv'>
+            <FaPlaneArrival className='faplane_icon' />
+            <input
+              type='text'
+              className='searchInput searchInput-2'
+              placeholder='Destino'
+              value={destination}
+              onChange={handleSubmit(setDestination)}
+            ></input>
+          </div>
+          <div className='inputDiv'>
+            <FaWpforms className='faplane_icon' />
+            <input
+              type='date'
+              className='searchInput searchInput-2'
+              placeholder='Fechas'
+              value={departureDate}
+              onChange={handleSubmit(setDepartureDate)}
+            ></input>
+          </div>
 
-          <input
-            type='text'
-            className='searchInput searchInput-2'
-            placeholder='Destino'
-            value={destination}
-            onChange={handleSubmit(setDestination)}
-          ></input>
-          <input
-            type='date'
-            className='searchInput searchInput-2'
-            placeholder='Fechas'
-            value={departureDate}
-            onChange={handleSubmit(setDepartureDate)}
-          ></input>
-
-          <Select
-            labelId='demo-simple-select-label'
-            id='demo-simple-select'
-            className='searchInput searchInput-2 adultsInput'
-            value={adults}
-            label='Age'
-            onChange={handleSubmit(setAdults)}
-          >
-            <MenuItem value={1}>1 adulto</MenuItem>
-            <MenuItem value={2}>2 adultos</MenuItem>
-            <MenuItem value={3}>3 adultos</MenuItem>
-            <MenuItem value={4}>4 adultos</MenuItem>
-            <MenuItem value={5}>5 adultos</MenuItem>
-            <MenuItem value={6}>6 adultos</MenuItem>
-          </Select>
+          <div className='inputDiv'>
+            <FaUserFriends className='faplane_icon' />
+            <select
+              className='searchInput searchInput-2 adultsInput'
+              value={adults}
+              onChange={handleSubmit(setAdults)}
+              placeholder='Adultos'
+            >
+              {ADULTS.map((adulto, key) => {
+                return (
+                  <option value={adulto.value} key={key}>
+                    {adulto.label}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
         </div>
         <Link to={url} className='btn btn-search' type='submit'>
           Buscar
