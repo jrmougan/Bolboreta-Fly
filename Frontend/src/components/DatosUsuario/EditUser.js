c1707c9301c169cbc071ebd401528898601dffa6
Jesús Romeo <jrmougan@gmail.com>
2022-02-21 17:10:07 +0100

    Instalación hook use-http

--- Frontend/src/components/DatosUsuario/EditUser.js
+++ Frontend/src/components/DatosUsuario/EditUser.js
@@ -1,44 +1,42 @@
 import "./style.css";
 
+import { useFetch } from "use-http";
+
 import React, { useState, useContext, useEffect } from "react";
 import decodeTokenData from "../../helpers/decodeTokenData";
 import useUserProfile from "../../hooks/useUserProfile";
 import { TokenContext } from "../../context/TokenContext";
 import swal from "sweetalert";
 import EditAvatar from "../EditAvatar/EditAvatar";
-import avataranonimo from '../../logos/photo.svg'
+import avataranonimo from "../../logos/photo.svg";
 import DeleteUsuario from "../DeleteUsuariio/DeleteUsuario";
 
-
 const EditUser = () => {
-    const [token, setToken] = useContext(TokenContext);
-    const [user, refetch, setRefetch] = useUserProfile(token);
-    const decodedToken = decodeTokenData(token);
-    const [newname, setNewname] = useState(user.userInfo?.name_user);
-    const [newlastname, setLastname] = useState(user.userInfo?.lastname);
-    const [newemail, setNewemail] = useState(user.userInfo?.email);
-    const [newbirthdate, setNewbirthdate] = useState(user.userInfo?.birthdate || new Date());
-    const [newaddress, setNewaddress] = useState(user.userInfo?.address);
-    const [newbio, setNewbio] = useState(user.userInfo?.bio);
+  const [token, setToken] = useContext(TokenContext);
+  //Se obtiene usuario con custom hook
+  const [user, refetch, setRefetch] = useUserProfile(token);
+  const decodedToken = decodeTokenData(token);
+  const [newname, setNewname] = useState(user.userInfo?.name_user);
+  const [newlastname, setLastname] = useState(user.userInfo?.lastname);
+  const [newemail, setNewemail] = useState(user.userInfo?.email);
+  const [newbirthdate, setNewbirthdate] = useState(
+    user.userInfo?.birthdate || new Date()
+  );
+  const [newaddress, setNewaddress] = useState(user.userInfo?.address);
+  const [newbio, setNewbio] = useState(user.userInfo?.bio);
 
-
-
-
-    console.log(decodedToken.id);
-    const updateUser = async (e) => {
-
-        e.preventDefault();
-        setRefetch(!refetch);
-        const newUser = {
-            newname: newname || user.userInfo?.name_user,
-            newlastname: newlastname || user.userInfo?.lastname,
-            newemail: newemail || user.userInfo?.email,
-            newbirthdate: newbirthdate || user.userInfo?.birthdate,
-            newaddress: newaddress || user.userInfo?.address,
-            newbio: newbio || user.userInfo?.bio,
-
-        }
-
+  const updateUser = async (e) => {
+    e.preventDefault();
+    setRefetch(!refetch);
+    const newUser = {
+      newname: newname || user.userInfo?.name_user,
+      newlastname: newlastname || user.userInfo?.lastname,
+      newemail: newemail || user.userInfo?.email,
+      newbirthdate: newbirthdate || user.userInfo?.birthdate,
+      newaddress: newaddress || user.userInfo?.address,
+      newbio: newbio || user.userInfo?.bio,
+    };
+    /*
         console.log(JSON.stringify(newUser));
         const res = await fetch(
             `http://${process.env.REACT_APP_PUBLIC_HOST_BACKEND}:${process.env.REACT_APP_PUBLIC_PORT_BACKEND}/user/${decodedToken?.id}/edit`,
@@ -60,118 +58,107 @@ const EditUser = () => {
             const error = await res.json();
             swal(error.message);
         }
+        */
+  };
 
-    }
+  const handleBirthdate = (e) => {
+    setNewbirthdate(e.target.value || user.userInfo?.birthdate);
+    console.log(e.target.value);
+  };
 
-    const handleBirthdate = (e) => {
-        setNewbirthdate(e.target.value || user.userInfo?.birthdate);
-        console.log(e.target.value)
-
-    }
-
-
-    return (
-        <div>
-            <form type="submit" id="getuser" onSubmit={updateUser}>
-                {" "}
-                <div className="fotocontainer">
-                    <img
-                        className="fotousuario"
-                        src={user.userInfo?.avatar ? `http://${process.env.REACT_APP_PUBLIC_HOST_BACKEND}:${process.env.REACT_APP_PUBLIC_PORT_BACKEND}/uploads/${user.userInfo?.avatar}` : avataranonimo}
-                        alt={`Avatar de ${user.userInfo?.name_user}`}
-                    />
-                </div>
-
-                <EditAvatar />
-
-                <h2> Datos de Usuario </h2>
-                <label htmlFor="name"> Nombre </label>
-                <input
-                    id="name"
-                    name="name"
-                    type="text"
-                    value={newname}
-                    onChange={(e) => {
-                        setNewname(e.target.value);
-                    }}
-                    placeholder={user.userInfo?.name_user}
-                />
-
-
-                <label htmlFor="lastname"> Apellido </label>
-                <input
-                    id="lastname"
-                    name="lastname"
-                    type="text"
-                    value={newlastname}
-                    onChange={(e) => {
-                        setLastname(e.target.value);
-
-                    }}
-                    placeholder={user.userInfo?.lastname}
-                />
-
-
-                <label htmlFor="email"> Email </label>
-                <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    value={newemail}
-                    onChange={(e) => {
-                        setNewemail(e.target.value);
-                    }}
-                    placeholder={user.userInfo?.email}
-                />
-
-
-                <label htmlFor="birthdate"> Fecha de Nacimiento </label>
-                <input placeholder={user.userInfo?.birthdate} />
-                <input
-                    id="birthdate"
-                    name="birthdate"
-                    type="date"
-                    value={newbirthdate}
-                    onChange={handleBirthdate}
-                />
-
-
-                <label htmlFor="address"> Dirección </label>
-                <input
-                    id="address"
-                    name="address"
-                    type="text"
-                    value={newaddress}
-                    onChange={(e) => {
-                        setNewaddress(e.target.value);
-                    }}
-                    placeholder={
-                        user.userInfo?.address ? user.userInfo?.address : "dirección"
-                    }
-                />
-
-
-                <label htmlFor="bio"> Biografía </label>
-                <textarea
-                    id="bio"
-                    name="bio"
-                    type="text"
-                    value={newbio}
-                    onChange={(e) => {
-                        setNewbio(e.target.value);
-
-                    }}
-                    placeholder={
-                        user.userInfo?.bio ? user.userInfo?.bio : "Cuentanos algo de ti"
-                    }
-                />
-
-                <button type="submit" className="guardarcambios"> Guardar Cambios  </button>
-            </form>
-            <DeleteUsuario />
+  return (
+    <div>
+      <form type="submit" id="getuser" onSubmit={updateUser}>
+        {" "}
+        <div className="fotocontainer">
+          <img
+            className="fotousuario"
+            src={
+              user.userInfo?.avatar
+                ? `http://${process.env.REACT_APP_PUBLIC_HOST_BACKEND}:${process.env.REACT_APP_PUBLIC_PORT_BACKEND}/uploads/${user.userInfo?.avatar}`
+                : avataranonimo
+            }
+            alt={`Avatar de ${user.userInfo?.name_user}`}
+          />
         </div>
-
-    );
+        <EditAvatar />
+        <h2> Datos de Usuario </h2>
+        <label htmlFor="name"> Nombre </label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          value={newname}
+          onChange={(e) => {
+            setNewname(e.target.value);
+          }}
+          placeholder={user.userInfo?.name_user}
+        />
+        <label htmlFor="lastname"> Apellido </label>
+        <input
+          id="lastname"
+          name="lastname"
+          type="text"
+          value={newlastname}
+          onChange={(e) => {
+            setLastname(e.target.value);
+          }}
+          placeholder={user.userInfo?.lastname}
+        />
+        <label htmlFor="email"> Email </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={newemail}
+          onChange={(e) => {
+            setNewemail(e.target.value);
+          }}
+          placeholder={user.userInfo?.email}
+        />
+        <label htmlFor="birthdate"> Fecha de Nacimiento </label>
+        <input placeholder={user.userInfo?.birthdate} />
+        <input
+          id="birthdate"
+          name="birthdate"
+          type="date"
+          value={newbirthdate}
+          onChange={handleBirthdate}
+        />
+        <label htmlFor="address"> Dirección </label>
+        <input
+          id="address"
+          name="address"
+          type="text"
+          value={newaddress}
+          onChange={(e) => {
+            setNewaddress(e.target.value);
+          }}
+          placeholder={
+            user.userInfo?.address ? user.userInfo?.address : "dirección"
+          }
+        />
+        <label htmlFor="bio"> Biografía </label>
+        <textarea
+          id="bio"
+          name="bio"
+          type="text"
+          value={newbio}
+          onChange={(e) => {
+            setNewbio(e.target.value);
+          }}
+          placeholder={
+            user.userInfo?.bio ? user.userInfo?.bio : "Cuentanos algo de ti"
+          }
+        />
+        <button type="submit" className="guardarcambios">
+          {" "}
+          Guardar Cambios{" "}
+        </button>
+      </form>
+      <DeleteUsuario />
+    </div>
+  );
 };
 
 export default EditUser;
--- Frontend/src/components/EditAvatar/EditAvatar.js
+++ Frontend/src/components/EditAvatar/EditAvatar.js
@@ -1,42 +1,39 @@
-
 import { React, useState, useContext, useEffect } from "react";
 import swal from "sweetalert";
-
+import { TokenContext } from "../../context/TokenContext";
+import decodeTokenData from "../../helpers/decodeTokenData";
+import useUserProfile from "../../hooks/useUserProfile";
 
 import "./style.css";
 
 const EditAvatar = () => {
+  const [token] = useContext(TokenContext);
+  const [user, avatar] = useUserProfile(token);
+  const decodedToken = decodeTokenData(token);
 
+  const newavatar = new FormData();
+  newavatar.append("avatar", avatar || user.userInfo?.avatar);
+  console.log(avatar);
+  console.log(newavatar);
 
-    const [token] = useContext(TokenContext);
-    const [user, avatar] = useUserProfile(token);
-    const decodedToken = decodeTokenData(token);
-    const [avatar, setAvatar] = useState(user.userInfo?.avatar);
+  const handleAvatar = (e) => {
+    console.log(e);
 
-    const newavatar = new FormData();
-    newavatar.append("avatar", avatar || user.userInfo?.avatar);
-    console.log(avatar);
-    console.log(newavatar);
+    //setAvatar(e.target.files[0]);
+  };
 
+  const fetchAvatar = () => {};
 
-
-    const handleAvatar = (e) => {
-        console.log(e);
-
-        setAvatar(e.target.files[0]);
-    };
-
-
-    return (
-        <div className="editavatarcontainer">
-            <label htmlFor="avatar"> Sube tu avatar</label>
-            <input id="avatar" name="avatar" type="file" onChange={handleAvatar} />
-            <button onClick={fetchAvatar} className="guardaravatar">
-                {" "}
-                Subir Avatar{" "}
-            </button>
-        </div>
-    );
+  return (
+    <div className="editavatarcontainer">
+      <label htmlFor="avatar"> Sube tu avatar</label>
+      <input id="avatar" name="avatar" type="file" onChange={handleAvatar} />
+      <button onClick={fetchAvatar} className="guardaravatar">
+        {" "}
+        Subir Avatar{" "}
+      </button>
+    </div>
+  );
 };
 
 export default EditAvatar;
