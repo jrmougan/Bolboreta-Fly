2b70bd4b03ca5d4bbe61e5dc3df50b7dc55342d8
Jesús Romeo <jrmougan@gmail.com>
2022-02-21 16:36:50 +0100

    refactorización avatar

--- Frontend/src/components/EditAvatar/EditAvatar.js
+++ Frontend/src/components/EditAvatar/EditAvatar.js
@@ -1,50 +1,25 @@
 
 import { React, useState, useContext, useEffect } from "react";
 import swal from "sweetalert";
-import decodeTokenData from "../../helpers/decodeTokenData";
-import useUserProfile from "../../hooks/useUserProfile";
-import { TokenContext } from "../../context/TokenContext";
+
+
 import "./style.css";
 
 const EditAvatar = () => {
 
 
     const [token] = useContext(TokenContext);
-    const [user] = useUserProfile(token);
+    const [user, avatar] = useUserProfile(token);
     const decodedToken = decodeTokenData(token);
     const [avatar, setAvatar] = useState(user.userInfo?.avatar);
 
+    const newavatar = new FormData();
+    newavatar.append("avatar", avatar || user.userInfo?.avatar);
+    console.log(avatar);
+    console.log(newavatar);
 
-    useEffect(() => { setAvatar(avatar) }, [avatar]);
 
 
-    const fetchAvatar = async (e) => {
-        e.preventDefault();
-
-        const newavatar = new FormData();
-        newavatar.append("avatar", avatar || user.userInfo?.avatar);
-        console.log(avatar);
-        console.log(newavatar);
-        const res = await fetch(
-            `http://${process.env.REACT_APP_PUBLIC_HOST_BACKEND}:${process.env.REACT_APP_PUBLIC_PORT_BACKEND}/user/${decodedToken?.id}/avatar`,
-            {
-                method: "PUT",
-                headers: {
-                    Authorization: token,
-                },
-                body: newavatar,
-            }
-        );
-
-        if (res.ok) {
-            const body = await res.json();
-            swal(body.message);
-        } else {
-            const error = await res.json();
-            swal(error.message);
-        }
-    };
-    
     const handleAvatar = (e) => {
         console.log(e);
 
