fd0e2d47b171bc1719cce693110e7c6d7bc44853
Jesús Romeo <jrmougan@gmail.com>
2022-03-27 21:16:05 +0200

    solucion useeffect autocomplete

--- Frontend/src/page/SearchRoot.js
+++ Frontend/src/page/SearchRoot.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import axios from 'axios';
-import SearchAutocomplete from '../components/CitySearch/components/search-autocomplete';
-import { getAmadeusData } from '../api/amadeus.api';
+import React from "react";
+import axios from "axios";
+import SearchAutocomplete from "../components/CitySearch/components/search-autocomplete";
+import { getAmadeusData } from "../api/amadeus.api";
 
 const SearchRoot = ({ setState, isOrigin }) => {
   const [search, setSearch] = React.useState({
-    keyword: 'a',
+    keyword: "",
     city: true,
     airport: true,
     page: 0,
@@ -18,36 +18,8 @@ const SearchRoot = ({ setState, isOrigin }) => {
 
   const [loading, setLoading] = React.useState(false);
 
-  React.useEffect(() => {
-    setLoading(true);
-
-    /* 
-       out - data del backend.
-       source - token para cancelación de la request
-    */
-    const { out, source } = getAmadeusData(search);
-
-    out
-      .then((res) => {
-        // Así prevenimos error en el lado del Cliente
-        if (!res.data.code) {
-          setDataSource(res.data);
-        }
-        setLoading(false);
-      })
-      .catch((err) => {
-        axios.isCancel(err);
-        setLoading(false);
-      });
-
-    // Cancelamos búsquedas
-    return () => {
-      source.cancel();
-    };
-  }, [search]);
-
   return (
-    <div className='search-panel inputDiv'>
+    <div className="search-panel inputDiv">
       <SearchAutocomplete
         search={search}
         setSearch={setSearch}
