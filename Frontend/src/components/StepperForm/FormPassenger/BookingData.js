53734480e8c1a8390785515ad18da7206f0ff425
Jesús Romeo <jrmougan@gmail.com>
2022-04-06 21:55:43 +0200

    estilos step 2

--- Frontend/src/components/StepperForm/FormPassenger/BookingData.js
+++ Frontend/src/components/StepperForm/FormPassenger/BookingData.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TextField } from "@mui/material";
+import { Grid, TextField } from "@mui/material";
 import "../style.css";
 
 const BookingData = (props) => {
@@ -12,23 +12,6 @@ const BookingData = (props) => {
 
 const ContactData = ({ state }) => {
   const [bookingData, setBookingData] = state;
-  console.log(bookingData);
-  const [nameBooking, setNameBooking] = useState("");
-  const [lastNameBooking, setLastNameBooking] = useState("");
-  const [lastNameBooking2, setLastnameBooking2] = useState("");
-  // const [birthdate, setBirhdate] = useState('');
-  const [typedocumentBooking, setTypeDocumentBooking] = useState("");
-  const [documentBooking, setDocumentBooking] = useState("");
-  const [addressBooking, setAddressBooking] = useState("");
-  const [countryBooking, setCountryBooking] = useState("");
-  const [cityBooking, setCityBooking] = useState("");
-  const [phoneBooking, setPhoneBooking] = useState("");
-
-  // Función para controlar los inputs
-  const handleSubmit = (setter) => (e) => {
-    // e.preventDefault();
-    setter(e.target.value);
-  };
 
   return (
     <div className="bookingData">
@@ -37,123 +20,153 @@ const ContactData = ({ state }) => {
         Introduce los datos de contacto para la reserva
       </h3>
       <form className="form_user">
-        <div className="identification_form">
-          <TextField
-            sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
-            required
-            id="Apellido-required"
-            placeholder="Nombre"
-            value={bookingData.name}
-            onChange={(e) => {
-              const modData = { ...bookingData };
-              modData.name = e.target.value;
-              setBookingData(modData);
-            }}
-            className="passenger-input"
-            margin="dense"
-          />{" "}
-          <TextField
-            sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
-            required
-            id="Apellido-required"
-            placeholder=" Primer Apellido"
-            value={bookingData.lastname}
-            onChange={(e) => {
-              const modData = { ...bookingData };
-              modData.lastname = e.target.value;
-              setBookingData(modData);
-            }}
-            className="passenger-input"
-            margin="dense"
-          />{" "}
-          <TextField
-            sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
-            required
-            id="Apellido-required"
-            placeholder="DNI / NIF / CIF / NIE"
-            value={bookingData.typedoc}
-            onChange={(e) => {
-              const modData = { ...bookingData };
-              modData.typedoc = e.target.value;
-              setBookingData(modData);
-            }}
-            className="passenger-input"
-            margin="dense"
-          />
-          <TextField
-            sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
-            required
-            id="Apellido-required"
-            placeholder="Introduzca aquí el número del documento"
-            value={bookingData.document}
-            onChange={(e) => {
-              const modData = { ...bookingData };
-              modData.document = e.target.value;
-              setBookingData(modData);
-            }}
-            className="passenger-input"
-            margin="dense"
-          />
-        </div>
+        <Grid container spacing={1} className="sectionForm">
+          <Grid xs={12}>
+            <h1>Datos personales</h1>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
+              required
+              label="Nombre"
+              id="Apellido-required"
+              placeholder="Nombre"
+              value={bookingData.name}
+              onChange={(e) => {
+                const modData = { ...bookingData };
+                modData.name = e.target.value;
+                setBookingData(modData);
+              }}
+              className="input"
+              margin="dense"
+            />{" "}
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
+              required
+              label="Apellido"
+              className="input"
+              id="Apellido-required"
+              placeholder=" Primer Apellido"
+              value={bookingData.lastname}
+              onChange={(e) => {
+                const modData = { ...bookingData };
+                modData.lastname = e.target.value;
+                setBookingData(modData);
+              }}
+              margin="dense"
+            />{" "}
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
+              required
+              id="Apellido-required"
+              placeholder="DNI / NIF / CIF / NIE"
+              value={bookingData.typedoc}
+              onChange={(e) => {
+                const modData = { ...bookingData };
+                modData.typedoc = e.target.value;
+                setBookingData(modData);
+              }}
+              label="Tipo de documento"
+              className="input"
+              margin="dense"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
+              required
+              id="Apellido-required"
+              placeholder="Introduzca aquí el número del documento"
+              value={bookingData.document}
+              onChange={(e) => {
+                const modData = { ...bookingData };
+                modData.document = e.target.value;
+                setBookingData(modData);
+              }}
+              label="Documento"
+              className="input"
+              margin="dense"
+            />
+          </Grid>
+        </Grid>
 
-        <div className="address_form">
-          <TextField
-            sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
-            required
-            id="Apellido-required"
-            placeholder="Dirección"
-            value={bookingData.address}
-            onChange={(e) => {
-              const modData = { ...bookingData };
-              modData.address = e.target.value;
-              setBookingData(modData);
-            }}
-            className="passenger-input"
-            margin="dense"
-          />{" "}
-          <TextField
-            sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
-            required
-            id="Country-required"
-            placeholder="País"
-            value={bookingData.country}
-            onChange={(e) => {
-              const modData = { ...bookingData };
-              modData.country = e.target.value;
-              setBookingData(modData);
-            }}
-            className="passenger-input"
-            margin="dense"
-          />{" "}
-          <TextField
-            sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
-            required
-            id="Apellido-required"
-            placeholder="Ciudad"
-            value={bookingData.city}
-            onChange={(e) => {
-              const modData = { ...bookingData };
-              modData.city = e.target.value;
-              setBookingData(modData);
-            }}
-            className="passenger-input"
-            margin="dense"
-          />{" "}
-          <TextField
-            sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
-            required
-            id="Apellido-required"
-            placeholder="Teléfono de contacto"
-            value={bookingData.phone}
-            onChange={(e) => {
-              const modData = { ...bookingData };
-              modData.phone = e.target.value;
-              setBookingData(modData);
-            }}
-            className="passenger-input"
-            margin="dense"
-          />
-        </div>
+        <Grid container spacing={1} className="sectionForm">
+          <Grid xs={12}>
+            <h1>Datos de contacto</h1>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
+              required
+              id="Apellido-required"
+              placeholder="Dirección"
+              value={bookingData.address}
+              onChange={(e) => {
+                const modData = { ...bookingData };
+                modData.address = e.target.value;
+                setBookingData(modData);
+              }}
+              label="Dirección"
+              className="input"
+              margin="dense"
+            />{" "}
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
+              required
+              id="Country-required"
+              placeholder="País"
+              value={bookingData.country}
+              onChange={(e) => {
+                const modData = { ...bookingData };
+                modData.country = e.target.value;
+                setBookingData(modData);
+              }}
+              label="País"
+              className="input"
+              margin="dense"
+            />{" "}
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
+              required
+              id="Apellido-required"
+              placeholder="Ciudad"
+              value={bookingData.city}
+              onChange={(e) => {
+                const modData = { ...bookingData };
+                modData.city = e.target.value;
+                setBookingData(modData);
+              }}
+              label="Ciudad"
+              className="input"
+              margin="dense"
+            />{" "}
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              sx={{ marginInline: "1rem", width: "30%", marginTop: "2rem" }}
+              required
+              id="Apellido-required"
+              placeholder="Teléfono de contacto"
+              value={bookingData.phone}
+              onChange={(e) => {
+                const modData = { ...bookingData };
+                modData.phone = e.target.value;
+                setBookingData(modData);
+              }}
+              label="Teléfono"
+              className="input"
+              margin="dense"
+            />
+          </Grid>
+        </Grid>
       </form>
     </div>
   );
