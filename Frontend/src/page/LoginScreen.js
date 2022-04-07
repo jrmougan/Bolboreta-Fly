c698ed54487fdf71ceb4a678d11daffcb3cdf3a8
Jesús Romeo <jrmougan@gmail.com>
2022-04-07 18:30:05 +0200

    Estilos login

--- Frontend/src/page/LoginScreen.js
+++ Frontend/src/page/LoginScreen.js
@@ -3,8 +3,6 @@ import LoginUser from "../components/LoginUser/LoginUser";
 const LoginScreen = () => {
   return (
     <div className="loginpagina">
-      <h3> Si eres usuario registrado de Bolboreta Flight.</h3>
-      <h1> Inicia Sesión</h1>
       <LoginUser />
     </div>
   );
