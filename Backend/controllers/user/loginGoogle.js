53fe93b1d7ab8d9dd579a2d1735e81a69285a3ef
Jesús Romeo <jrmougan@gmail.com>
2022-04-07 19:35:19 +0200

    Arreglado login google connection release

--- Backend/controllers/user/loginGoogle.js
+++ Backend/controllers/user/loginGoogle.js
@@ -1,61 +1,68 @@
 const { OAuth2Client } = require('google-auth-library');
 const getDB = require('../../database/getDB');
 const jwt = require('jsonwebtoken');
+const { finance } = require('faker/lib/locales/en');
 
 const client = new OAuth2Client(process.env.APP_GOOGLE_CLIENT_ID);
 
 const loginGoogle = async (req, res, next) => {
     let connection;
 
-    connection = await getDB();
+    try {
+        connection = await getDB();
 
-    const { token } = req.body;
+        const { token } = req.body;
 
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        requireAudience: process.env.APP_GOOGLE_CLIENT_ID,
-    });
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            requireAudience: process.env.APP_GOOGLE_CLIENT_ID,
+        });
 
-    const { given_name, email, family_name } = ticket.getPayload();
+        const { given_name, email, family_name } = ticket.getPayload();
 
-    const [user] = await connection.query(
-        `SELECT id FROM user WHERE email = ?`,
-        [email]
-    );
-
-    if (user.length > 0) {
-        await connection.query(
-            `UPDATE user SET name_user='${given_name}' WHERE email = ?`,
+        const [user] = await connection.query(
+            `SELECT id FROM user WHERE email = ?`,
             [email]
         );
-    }
-    if (user.length < 1) {
-        await connection.query(
-            `INSERT INTO  user (name_user, lastname, email , password, createDate , active  ) VALUES(?,?,?,?,?,?)`,
-            [given_name, family_name, email, email, new Date(), 1]
+
+        if (user.length > 0) {
+            await connection.query(
+                `UPDATE user SET name_user='${given_name}' WHERE email = ?`,
+                [email]
+            );
+        }
+        if (user.length < 1) {
+            await connection.query(
+                `INSERT INTO  user (name_user, lastname, email , password, createDate , active  ) VALUES(?,?,?,?,?,?)`,
+                [given_name, family_name, email, email, new Date(), 1]
+            );
+        }
+
+        //comprbar si el email existe
+        const [usergoogle] = await connection.query(
+            `SELECT id , rol , password, active FROM user WHERE email = ?`,
+            [email]
         );
+
+        //Creamos nuestro propio token para enviarselo a nuestros middelwares y al frontend
+
+        const tokenInfo = {
+            id: usergoogle[0].id,
+            rol: usergoogle[0].rol,
+            active: 1,
+        };
+
+        const tokenGoogle = jwt.sign(tokenInfo, process.env.SECRET, {
+            expiresIn: '10d',
+        });
+
+        res.status(201);
+        res.json(tokenGoogle);
+    } catch (error) {
+        next(error);
+    } finally {
+        if (connection) connection.release();
     }
-
-    //comprbar si el email existe
-    const [usergoogle] = await connection.query(
-        `SELECT id , rol , password, active FROM user WHERE email = ?`,
-        [email]
-    );
-
-    //Creamos nuestro propio token para enviarselo a nuestros middelwares y al frontend
-
-    const tokenInfo = {
-        id: usergoogle[0].id,
-        rol: usergoogle[0].rol,
-        active: 1,
-    };
-
-    const tokenGoogle = jwt.sign(tokenInfo, process.env.SECRET, {
-        expiresIn: '10d',
-    });
-
-    res.status(201);
-    res.json(tokenGoogle);
 };
 
 module.exports = loginGoogle;
